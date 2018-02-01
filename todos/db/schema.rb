90417bc4ac7645989fa86a126f814d8da83a6ca6
Annie Polish <annie.polish@yale.edu>
2018-02-01 13:28:11 -0500

    todos scoped to users

--- todos/db/schema.rb
+++ todos/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127215440) do
+ActiveRecord::Schema.define(version: 20180128184859) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
